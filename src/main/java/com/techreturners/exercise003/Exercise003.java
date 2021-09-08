dcc530c584c74afd7c66f6194f0bf32142fef248
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-08 01:14:52 +0100

    exercise003 completed

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,31 @@
 package com.techreturners.exercise003;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise003 {
 
+    Map<String, Integer> iceCreamFlavourMap = new HashMap<String, Integer>() {
+        {
+            put("Pistachio", 0);
+            put("Raspberry Ripple", 1);
+            put("Vanilla", 2);
+            put("Mint Chocolate Chip", 3);
+            put("Chocolate", 4);
+            put("Mango Sorbet", 5);
+        }
+    };
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return iceCreamFlavourMap.get(iceCreamFlavour);
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
-    }
+        String[] flavours = new String[iceCreamFlavourMap.size()];
+        for (String key : iceCreamFlavourMap.keySet()) {
+            flavours[iceCreamFlavourMap.get(key)] = key;
+        }
+        return flavours;
 
+    }
 }
