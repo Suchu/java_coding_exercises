a11edf01f355fd436a82c2686893297f5743fe13
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-09 01:43:26 +0100

    Implemented exercise005 IsPangram

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,13 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        input = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (!input.contains(String.valueOf(ch)))
+                return false;
+        }
+        return true;
     }
 
 }
